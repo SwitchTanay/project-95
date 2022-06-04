834ce8294fad3535fdc9f3d6d34f71ac9c13a6bf
SwitchTanay <86693362+SwitchTanay@users.noreply.github.com>
2022-06-04 15:50:15 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmHZOwd_bZ_1e4fjOwG2QTFRRBvq6iDnw",
+    authDomain: "let-s-chat-project-3327f.firebaseapp.com",
+    databaseURL: "https://let-s-chat-project-3327f-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-project-3327f",
+    storageBucket: "let-s-chat-project-3327f.appspot.com",
+    messagingSenderId: "444463805044",
+    appId: "1:444463805044:web:3963e8fd6708fb4ae21721",
+    measurementId: "G-BQECQEVGFL"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
